feb74bd9f4490d7772af5a36a58d9c0e68cdc1eb
gunnerr91 <blissvortex@hotmail.com>
2020-07-09 21:23:03 +0100

    added splash screen, player object and music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        LoadFirstLevel();
+    }
+
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
