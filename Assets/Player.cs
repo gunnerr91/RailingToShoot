79d29c674b13d298dd6e1ba4d203c630295e1dc5
gunnerr91 <blissvortex@hotmail.com>
2020-07-10 22:48:36 +0100

    added script to calculate delta time

---
+++ Assets/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        var xoffSet = horizontalThrow * speed  *  Time.deltaTime;
+        print(xoffSet);
+    }
+}
