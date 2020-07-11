99e5c0cc64cc6d9cc41861756745076b66706973
gunnerr91 <blissvortex@hotmail.com>
2020-07-11 19:31:40 +0100

    configured vertical, horizontal movement and added rotation when moving

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float speed = 4f;
+    [SerializeField] float speed = 20f;
+    [SerializeField] float xRange = 8f;
+    [SerializeField] float yRange = 8f;
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float rotationPitchFactor = -30f;
+
+    [SerializeField] float positionYawFactor = 1.5f;
+
+    [SerializeField] float rotationRollFactor = 20f;
+
+    float horizontalThrow, verticalThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +28,31 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
-        var xoffSet = horizontalThrow * speed  *  Time.deltaTime;
-        print(xoffSet);
+        ProcessPlayerPosition();
+        ProcessPlayerRotation();
+    }
+
+    private void ProcessPlayerRotation()
+    {
+
+        float pitch = transform.localPosition.y * positionPitchFactor + verticalThrow * rotationPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = horizontalThrow * rotationRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessPlayerPosition()
+    {
+        horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        var xOffSet = horizontalThrow * speed * Time.deltaTime;
+        var rawXPosition = transform.localPosition.x + xOffSet;
+        var clampRawXPosition = Mathf.Clamp(rawXPosition, -xRange, xRange);
+
+        verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        var yOffSet = verticalThrow * speed * Time.deltaTime;
+        var rawYPosition = transform.localPosition.y + yOffSet;
+        var clampRawYPosition = Mathf.Clamp(rawYPosition, -yRange, yRange);
+
+        transform.localPosition = new Vector3(clampRawXPosition, clampRawYPosition, transform.localPosition.z);
     }
 }
