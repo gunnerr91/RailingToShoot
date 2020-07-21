db7a1c19ff2bad2f69e918ad87ca58ddab1eaf6a
gunnerr91 <blissvortex@hotmail.com>
2020-07-21 08:51:48 +0100

    added new font and scoreboard script

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 12;
+
+    int score;
+    Text scoreTextComponent;
+
+    void Start()
+    {
+        scoreTextComponent = GetComponent<Text>();
+        scoreTextComponent.text = score.ToString();
+    }
+
+    public void UpdateScore()
+    {
+        scoreTextComponent.text = (score += scorePerHit).ToString();
+    }
+}
